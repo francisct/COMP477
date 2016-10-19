45135417aedbc2e24c13d9d48a0021d15142faee
francisct <franciscotetremblay@gmail.com>
2016-10-19 10:50:23 -0400

    Added basic movement script Added template scripts for legs and arms and limbs (abstract) Added material to ragdoll for better visbility

---
+++ Assets/Scripts/Abstracts/LimbMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LimbMovement : MonoBehaviour {
+
+    public Transform leftLimb;
+    public Transform rightLimb;
+
+	// Use this for initialization
+	protected virtual void Start () {
+	
+	}
+
+    // Update is called once per frame
+    protected virtual void Update () {
+	
+	}
+
+    public virtual void Left()
+    {
+
+    }
+
+    public virtual void Right()
+    {
+
+    }
+}
